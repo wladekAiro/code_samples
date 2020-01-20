66f133f6d968417d98361c88eedb09d468d086d1
Wladek Airo <wladek.airo@gmail.com>
2020-01-20 16:45:16 +0300

    unix watcher

---
+++ node/networking/net-watcher-unix.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fs =  require('fs');
+const net  =  require('net'); // Nodejs networking module
+const fileName = process.argv[2];
+
+if (!fileName){
+    throw Error(` Error: No filename specified !!! `);
+}
+
+net.createServer(connection => {
+    // Reporting
+    console.log(' Subscriber connected ');
+    connection.write(` now watching ${fileName} for changes ... \n`);
+
+    //Watcher setup
+    const watcher = fs.watch(fileName, () => connection.write(` File changed: ${new Date()} \n`));
+
+    //clean up
+    connection.on('close', () => {
+        console.log(' Subscriber disconnected. \n');
+        watcher.close();
+    });
+}).listen('/tmp/watcher.sock',() => {
+    console.log(' Listening for subscribers ... ');
+});
