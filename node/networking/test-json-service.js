153d071372da01ec8e9f057781daf8dc338d370b
Wladek Airo <wladek.airo@gmail.com>
2020-01-27 13:02:21 +0300

    test chunked json

---
+++ node/networking/test-json-service.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const server = require('net').createServer(connection => {
+    console.log(' Subscriber connected');
+    //Two message chunks that together make a whole message
+    const firstChunk = `{"type":"changed", "timesta'`;
+    const secondChunk = `'mp":1450694370094}\n`;
+
+    //send first chunk immediately
+    connection.write(firstChunk);
+
+    //send second chunk after a short delay
+    const timer = setTimeout(() => {
+        connection.write(secondChunk);
+        connection.end();
+    }, 100);
+
+    //clear timer when timer ends
+    connection.on('end', () => {
+        //clear timer
+        clearTimeout(timer);
+        console.log(' Subscriber disconnected ');
+    });
+});
+
+server.listen(60300, () => {
+    console.log('Server listening for subscribers');
+});
