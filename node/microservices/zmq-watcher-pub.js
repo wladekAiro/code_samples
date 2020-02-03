ea76ebde0c406a1065b56194425a580923eb5223
Wladek Airo <wladek.airo@gmail.com>
2020-02-03 17:02:18 +0300

    zmq file watcher publisher

---
+++ node/microservices/zmq-watcher-pub.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fs = require('fs');
+const zmq = require('zeromq');
+const fileName = process.argv[2];
+
+//Create publisher endpoint
+const publisher = zmq.socket('pub');
+
+fs.watch(fileName,() => {
+    //Send a message to any and all subscribers
+    publisher.send(JSON.stringify({
+        type: 'changed',
+        file: fileName,
+        timestamp : Date.now()
+    }));
+});
+
+//listen to tcp port 60400
+publisher.listen('tcp://*:60400', err => {
+    if (err){
+        throw err;
+    }
+
+    console.log(' Listening for zmq subscribers ');
+});
