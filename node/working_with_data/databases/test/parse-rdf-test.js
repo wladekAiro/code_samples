3d1a0fdfed0afd000e0c8fbf6c4bc434e502abb8
Wladek Airo <wladek.airo@gmail.com>
2020-03-09 16:02:32 +0300

    first failing mocha test with chai.

---
+++ node/working_with_data/databases/test/parse-rdf-test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fs = require('fs');
+const mocha = require("mocha");
+const expect = require('chai').expect;
+
+const rdf = fs.readFileSync(`${__dirname}/pg132.rdf`);
+
+mocha.describe('parseRDF', () => {
+    mocha.it('should should be a function', () => {
+        expect(parseRDF).to.be.a('function');
+    });
+});
+
